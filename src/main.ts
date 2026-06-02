4908aeaea1b1fe609d46cda77a2dfc86fb3910d6
Dylan Whitney <e9004541@mac.lan>
2026-06-02 09:50:47 -0500

    Adding New Relic

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,53 @@
 import { ViteSSG } from 'vite-ssg'
+import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
 
 import './style.css';
 import App from './App.vue';
 import { routes } from './lib/routes';
 import { trackPageView } from './lib/analytics';
 
+const options = {
+  "info": {
+    "applicationID": 653419329,
+    "beacon": "bam.nr-data.net",
+    "errorBeacon": "bam.nr-data.net",
+    "licenseKey": "NRJS-8688cc793ecfb998d0b",
+    "sa": 1
+  },
+  "init": {
+    "ajax": {
+      "deny_list": [
+        "bam.nr-data.net"
+      ]
+    },
+    "browser_consent_mode": {
+      "enabled": false
+    },
+    "distributed_tracing": {
+      "enabled": true
+    },
+    "performance": {
+      "capture_detail": false,
+      "capture_marks": false,
+      "capture_measures": true
+    },
+    "privacy": {
+      "cookies_enabled": true
+    }
+  },
+  "loader_config": {
+    "accountID": 8127277,
+    "agentID": 653419329,
+    "applicationID": 653419329,
+    "licenseKey": "NRJS-8688cc793ecfb998d0b",
+    "trustKey": 8127277
+  }
+}
+
+// The agent loader code executes immediately on instantiation.
+const nrba = new BrowserAgent(options)
+
+// Remaining code
 export const createApp = ViteSSG(
   App,
   {
