abe1f218288030076deb2c5574156cd3a8fda64d
Dylan Whitney <thedylanwhitney@gmail.com>
2026-07-28 17:40:16 -0500

    Center modal

--- src/components/JuliaModal.vue
+++ src/components/JuliaModal.vue
@@ -9,7 +9,7 @@
     <Transition name="modal-panel">
       <div v-if="open" class="fixed inset-0 z-floating overflow-y-auto" @click.self="close">
         <div
-          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+          class="flex min-h-full items-center justify-center p-4 text-center sm:p-0"
           @click.self="close"
         >
           <div
